8ce0466edde3f41aa5c204d7ca0ef60bca1833a6
Luiz Bills <luizbills@pm.me>
2025-07-03 14:38:05 -0300

    add "ping" demo

---
+++ samples/examples/ping/README.md
@@ -0,0 +1,5 @@
+# ping
+
+This example shows how to calculate the current latency.
+
+> Try this example in two browser windows at once!
---
+++ samples/examples/ping/index.js
@@ -0,0 +1,64 @@
+let shared
+let ping = false
+
+function setup() {
+  // const url = "ws://0.0.0.0:6020"
+  const url = "wss://vanilla-party.luizbills.com"
+
+  partyConnect(url, "test_ping", "main", () => {
+    // tracker = new PingTracker()
+    partyTrackPing((value) => {
+      ping = value
+    })
+  })
+
+  createCanvas(400, 400)
+  noStroke()
+  partyToggleInfo(true)
+}
+
+function draw() {
+  background("white")
+
+  textSize(20)
+  text(
+    "PING: " +
+      (ping !== false ? `${ping} ms (${(ping / 1000).toFixed(2)} s)` : "..."),
+    50,
+    50
+  )
+  return
+}
+
+function partyTrackPing(cb, interval = 1000) {
+  const id = Math.random()
+
+  let currentPing = 0
+
+  const onPing = function (data) {
+    if (partyIsHost()) {
+      partyEmit("pong", data)
+    }
+  }
+
+  const onPong = function (data) {
+    if (data.sender === id) {
+      setTimeout(emitPing, interval)
+      const prev = currentPing
+      currentPing = performance.now() - data.time
+      cb(currentPing, prev)
+    }
+  }
+
+  const emitPing = function () {
+    partyEmit("ping", {
+      sender: id,
+      time: performance.now(),
+    })
+  }
+
+  partySubscribe("ping", onPing)
+  partySubscribe("pong", onPong)
+
+  setTimeout(emitPing, interval)
+}
