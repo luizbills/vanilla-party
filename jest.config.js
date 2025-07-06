6a60323ad88f6f426aa165fe5139d0361a2743c5
Luiz Bills <luizbills@pm.me>
2025-07-06 17:52:19 -0300

    disable code covarage in jest

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,6 @@
 export default {
   preset: "ts-jest/presets/js-with-ts",
   testEnvironment: "jsdom", // requires jest-environment-jsdom
-  collectCoverage: true,
-  coverageReporters: ["text", "json-summary"],
-
   transformIgnorePatterns: ["/node_modules/(?!(on-change)/)"],
-
   testRegex: "((\\.|/*.)(test))\\.[jt]sx?$",
 }
